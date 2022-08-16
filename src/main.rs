b5dfd0820384f7e927bb720a99ee737142fddbf3
차영곤 <garmede@gmail.com>
2022-08-16 14:54:52 +0900

    first image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    // Image
+
+    let imgx = 256;
+    let imgy = 256;
+
+    // Render
+    
+    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
+
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let r = (x as f32) as u8;
+        let g = (256.0 - y as f32) as u8;
+        *pixel = image::Rgb([r, g, 64]);
+    }
+
+    imgbuf.save("render/first_image.png").unwrap();
 }
